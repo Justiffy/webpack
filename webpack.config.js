daaeac8ba4eee9d21529a648b7620a2bcb84eb2a
Listar <shnap2010@yandex.ru>
2020-02-25 00:14:04 +0500

    add cor lib

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+  mode: "production",
+  entry: './src/index.tsx',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devtool: "source-map",
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".jsx"]
+  },
+
+  module: {
+		rules: [
+			{
+				test: /\.ts(x?)$/,
+				exclude: /node_modules/,
+				use: [
+					{
+							loader: "ts-loader"
+					}
+				]
+			},
+		]
+  },
+};
