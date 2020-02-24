95d52de0c6296110dc3ff7b5bcc3a827f28c33ed
Listar <shnap2010@yandex.ru>
2020-02-25 00:53:59 +0500

    add svg loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import * as React from 'react';
 
+import Icon from './assets/Boom.svg'
+
 import './App.css'
 
 function App() {
   return(
     <div className="wrapper">
       Listar ?!
+      <Icon width={24} height={24} />
     </div>
   )
 }
--- webpack.config.js
+++ webpack.config.js
@@ -30,6 +30,18 @@ module.exports = {
           'css-loader',
         ],
       },
+      {
+        test: /\.svg$/,
+        use: [
+          {
+            loader: "babel-loader"
+          },
+          {
+            loader: "react-svg-loader",
+            options: {},
+          }
+        ]
+      }
 		]
   },
 };
