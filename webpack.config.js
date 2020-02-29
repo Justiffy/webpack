df93dabe09f9e31a71c88d897b507885cb45362c
Listar <shnap2010@yandex.ru>
2020-02-29 22:34:39 +0500

    env var

--- webpack.config.js
+++ webpack.config.js
@@ -2,75 +2,77 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-module.exports = {
-  mode: 'development',
-  devtool: 'inline-source-map',
-  entry: './src/index.tsx',
-  output: {
-    filename: '[name].[contenthash].js',
-    chunkFilename: '[name].[contenthash].js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      template: './index.html',
-    }),
-  ],
-
-  optimization: {
-    moduleIds: 'hashed',
-    runtimeChunk: 'single',
-    splitChunks: {
-      cacheGroups: {
-        vendor: {
-          test: /[\\/]node_modules[\\/]/,
-          name: 'vendors',
-          chunks: 'all',
-        },
-      }
-    } 
-  },
-
-  devServer: {
-    contentBase: './dist',
-  },
+module.exports = env => {
+  return {
+    mode: 'development',
+    devtool: 'inline-source-map',
+    entry: './src/index.tsx',
+    output: {
+      filename: '[name].[contenthash].js',
+      chunkFilename: '[name].[contenthash].js',
+      path: path.resolve(__dirname, 'dist'),
+    },
   
-  resolve: {
-    extensions: [".ts", ".tsx", ".js", ".jsx"]
-  },
-
-  module: {
-		rules: [
-			{
-				test: /\.ts(x?)$/,
-				exclude: /node_modules/,
-				use: [
-					{
-							loader: "ts-loader"
-					}
-				]
-      },
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader',
-        ],
-      },
-      {
-        test: /\.svg$/,
-        use: [
-          {
-            loader: "babel-loader"
+    plugins: [
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        template: './index.html',
+      }),
+    ],
+  
+    optimization: {
+      moduleIds: 'hashed',
+      runtimeChunk: 'single',
+      splitChunks: {
+        cacheGroups: {
+          vendor: {
+            test: /[\\/]node_modules[\\/]/,
+            name: 'vendors',
+            chunks: 'all',
           },
-          {
-            loader: "react-svg-loader",
-            options: {},
-          }
-        ]
-      }
-		]
-  },
+        }
+      } 
+    },
+  
+    devServer: {
+      contentBase: './dist',
+    },
+    
+    resolve: {
+      extensions: [".ts", ".tsx", ".js", ".jsx"]
+    },
+  
+    module: {
+      rules: [
+        {
+          test: /\.ts(x?)$/,
+          exclude: /node_modules/,
+          use: [
+            {
+                loader: "ts-loader"
+            }
+          ]
+        },
+        {
+          test: /\.css$/,
+          use: [
+            'style-loader',
+            'css-loader',
+          ],
+        },
+        {
+          test: /\.svg$/,
+          use: [
+            {
+              loader: "babel-loader"
+            },
+            {
+              loader: "react-svg-loader",
+              options: {},
+            }
+          ]
+        }
+      ]
+    },
+  };
 };
